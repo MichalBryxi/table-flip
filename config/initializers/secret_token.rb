26a0b8cbbe3e2447978c10cf7665a943c5d32590
Michal Bryxí <michal.bryxi@gmail.com>
2014-04-28 23:45:48 +0200

    Initial Rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TableflipPudrCom::Application.config.secret_key_base = 'dad611ba3af39b129a55ea9cf6ef4350e7086583e995088a415585194623a6f13e8fc91e47eb5f5b049c547d5264ac7ecb39187695145b10ffa8e15417a55541'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TableflipPudrCom::Application.config.session_store :cookie_store, key: '_tableflip_pudr_com_session'
